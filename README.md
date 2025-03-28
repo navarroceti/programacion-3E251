d28fd9eb646bb37c7dad3be0d9e9833d29583a6c
navarroceti <129806280+navarroceti@users.noreply.github.com>
2025-03-28 03:29:43 +0000

    practica 2

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # programacion-3E251
+
+c++ main.cpp -o programa -lftxui-component -lftxui-dom -lftxui-screen -std=c++17
+
+./programa
--- main.cpp
+++ main.cpp
@@ -1,16 +1,34 @@
 #include "Serie.hpp"
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/dom/elements.hpp>
+#include <iostream>
+using namespace std;
+using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    Serie serieNavidenia;
+    auto pantalla = Screen::Create(
+        Dimension::Full(), 
+        Dimension::Full()
+    );
+
+    auto documento = vbox(
+        spinner(21,1)
+    );
+
+    Render(pantalla, documento);
+    pantalla.Print();
+
+
+    // Serie serieNavidenia;
     
-    serieNavidenia.Imprimir();
-    serieNavidenia.EncenderTodo();
-    serieNavidenia.Imprimir();
-    serieNavidenia.ApagarCantidad(5);
-    serieNavidenia.Imprimir();
-    serieNavidenia.ApagarTodo();
-    serieNavidenia.Imprimir();
+    // serieNavidenia.Imprimir();
+    // serieNavidenia.EncenderTodo();
+    // serieNavidenia.Imprimir();
+    // serieNavidenia.ApagarCantidad(5);
+    // serieNavidenia.Imprimir();
+    // serieNavidenia.ApagarTodo();
+    // serieNavidenia.Imprimir();
 
     return 0;
 }
